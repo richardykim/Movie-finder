2099d63f6b55183ce6e97c0f8ed3561dc7670e54
richardykim <richard.y.kim4@gmail.com>
2017-07-23 21:37:42 -0400

    add function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar'
-import logo from './logo.svg';
 import './App.css';
 
 const API_KEY = 'ac9fcc068de5367719b98d63133cbf6d'
-
+const MovieDB = require('moviedb')+ API_KEY;
 
 class App extends Component {
 
   videoSearch(term){
-    
+    MovieDB.searchMovie({ query: 'Alien' }, (err, res) => {
+      console.log(res);
+    });
   }
+
 
   render() {
     return (
       <div className="App">
         <div className="container">
             <h1>Welcome to the show finder</h1>
-            <SearchBar />
+            <SearchBar onSearchTermChange={videoSearch}/>
         </div>
       </div>
     );
--- src/search_bar.js
+++ src/search_bar.js
@@ -14,9 +14,15 @@ class SearchBar extends Component{
         <input
           placeholder="Search show"
           value = {this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
           />
       </div>
     )
+  }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
